ed04f7960277e699b16ad849298eed450eee81f8
mi-wada <mtak.wd@gmail.com>
2025-02-06 22:47:50 +0900

    fix package comment

--- cachecontrolheader.go
+++ cachecontrolheader.go
@@ -1,5 +1,6 @@
 // Package cachecontrolheader provides functionality to parse and handle
-// Cache-Control headers based on [RFC9111](https://datatracker.ietf.org/doc/html/rfc9111.html#name-cache-control).
+// Cache-Control headers based on RFC9111.
+// https://datatracker.ietf.org/doc/html/rfc9111.html#name-cache-control
 package cachecontrolheader
 
 import (
